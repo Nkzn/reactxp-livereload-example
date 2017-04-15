9af641350d48f48ba63fe9655f7064d90c16c618
Yukiya Nakagawa <yn.airscope@gmail.com>
2017-04-15 23:58:03 +0900

    enable dev-server & live reload

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,31 @@
+// For hot reload settings
+declare var module: { hot: any };
+declare var require: {
+    (path: string): any;
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};
+
 import RX = require('reactxp');
 import App = require('./App');
 
 RX.App.initialize(true, true);
-RX.UserInterface.setMainView(<App />);
+
+if (RX.Platform.getType() === "web") {
+    const { AppContainer } = require("react-hot-loader");
+    const render = (App: any) => RX.UserInterface.setMainView(
+        <AppContainer>
+            <App />
+        </AppContainer>
+    );
+
+    if (module.hot) {
+        module.hot.accept();
+        render(App)
+    } else {
+        render(App)
+    }
+} else {
+    RX.UserInterface.setMainView(<App />);
+}
